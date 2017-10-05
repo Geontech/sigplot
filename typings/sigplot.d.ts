651b661c76a3f43b11e2e7e57fbf50d26f4a9a91
Thomas Goodwin <btgoodwin@geontech.com>
2017-10-05 15:50:12 -0400

    Minor TSlint related formatting, a few memberes changed types in the settings interfaces
    Also, exported the layers by name so that the member works correctly in TS.

--- typings/sigplot.d.ts
+++ typings/sigplot.d.ts
@@ -6,6 +6,9 @@ export * from './sigplot.layer';
 export * from './sigplot.layer1d';
 export * from './sigplot.layer2d';
 
+export { Layer1D } from './sigplot.layer1d';
+export { Layer2D } from './sigplot.layer2d';
+
 import { m } from './m';
 import { bluefile } from './bluefile';
 import { layer } from './sigplot.layer';
@@ -13,7 +16,7 @@ import { layer } from './sigplot.layer';
 /**
  * Purpose: Construction Options interface
  */
-export interface IConstructorOptions {
+export interface IConstructorOptions extends ISettingsOptions {
     /**
      * Note: See CMode Enumeration
      * the plot rendering mode for incoming data:
@@ -30,7 +33,7 @@ export interface IConstructorOptions {
      *   RI : Real vs. Imaginary
      *   IR : Imaginary vs. Real
      */
-    cmode? : string,
+    cmode?:  string,
 
     /**
      * Note: See PHUnits Enumeration
@@ -39,108 +42,112 @@ export interface IConstructorOptions {
      *   R : Radians
      *   C : Cycles 
      */
-    phunits? : string,
+    phunits?:  string,
 
     /**
      * enable crosshairs on plot
      */
-    cross? :  boolean,
+    cross?:   boolean,
 
     /**
      * set to true to hide grid on plot
      */
-    nogrid? : boolean,
+    nogrid?:  boolean,
+
+    gridBackground?:  string | Array<string>, // HTML5 canvas fillStyle (so "red" or an array, etc.)
+    gridStyle?:  IGridStyle,
+    fillStyle?:  string | Array<string>,
 
     /** 
      * set to false to hide legend on plot
      */
-    legend? : boolean,
+    legend?:  boolean,
 
     /**
-     * set to true to hide legend on plot
+     * set true to hide the legend button on the plot
      */
-    no_legend? : boolean,
+    no_legend_button?:  boolean,
 
     /**
      * set to true to disable panning on the plot
      */
-    nopan? : boolean,
+    nopan?:  boolean,
 
     /**
      * set to true to disable the menu on middle-click
      */
-    nomenu? : boolean,
+    nomenu?:  boolean,
 
         /**
     * set to true to disable the plot specification displays
     */
-    nospec? : boolean,
+    nospecs?:  boolean,
 
     /**
      * set to true to disable the x-axis labels
      */
-    noxaxis? : boolean,
+    noxaxis?:  boolean,
 
     /**
      * set to true to disable the y-axis labels
      */
-    noyaxis? : boolean,
+    noyaxis?:  boolean,
 
     /**
      * set to true to hide the plot readout displays
      */
-    noreadout? : boolean,
+    noreadout?:  boolean,
 
     /**
      * set to true to disable the ability to drag-drop files onto the plot
      */
-    nodragdrop? : boolean,
+    nodragdrop?:  boolean,
 
     /**
      * TODO
      */
-    scroll_time_interval? : number,
+    scroll_time_interval?:  number,
 
     /**
      * on the readout, show the index of the sample instead of the abscissa offset
      */
-    index? : boolean,
+    index?:  boolean,
 
     /**
      * Note: See AutoScale enumeration
      * auto-scaling mode for the xaxis
-     *    0, "fix"  : axis is fixed to ymin and ymax
-     *    1, "min"  : maximum is fixed, minimum is automatic
-     *    2, "max"  : minimum is fixed, maximum is automatic
+     *    0, "fix":  axis is fixed to ymin and ymax
+     *    1, "min":  maximum is fixed, minimum is automatic
+     *    2, "max":  minimum is fixed, maximum is automatic
      *    3, "auto" : both minimum and maximum are automatic
      */
-    autox? : string | number,
+    autox?:  string | number,
 
     /**
     * when auto-scaling for the x minimum is turned off, set the xmin value with this
     */
-    xmin? :  number,
+    xmin?:   number,
 
     /**
     * when auto-scaling for the x maximum is turned off, set the xmin value with this
     */
-    xmax? : number,
+    xmax?:  number,
 
     /**
     * Note: See MUnits enumeration
     * the units for the x axis (looked up using m.UNITS)
     */
-    xlab? : string | number,
+    xlab?:  string | number,
 
     /**
     * the units for the x axis
     */
-    xlabel? : string | ((...args: any[]) => string), 
+    xlabel?:  string | ((...args: any[]) => string), 
 
     /**
     * the number of divisions to use on the x axis labels
     */
-    xdiv? :  number,
+    xdiv?:   number,
 
     /**
     * configure the behaviour of the mouse controls:
@@ -148,22 +155,34 @@ export interface IConstructorOptions {
     *   1 : (default) left-mouse clicks set "tag"
     *   2 : left-mouse movements sets "tag"
     */
-    xcnt? : number,
+    xcnt?:  string,
+
+    nsec?:      number,
+    anno_type?: number,
+    ncolors?:   number,
+    always_show_marker?: boolean,
+    autohide_readout?: boolean,
+    autohide_panbars?: boolean,
+    hide_note?: boolean,
+
+    x_scrollbar_location?: any,
 
     /**
     * the behaviour of the drawing a box with the left-mouse
     *   zoom : (default) zoom on the plot
-    *   box  : send a mouse box event
+    *   box:  send a mouse box event
     */
-    rubberbox_mode? : string,
+    rubberbox_mode?:  string,
+    rubberbox_action?:  string,
 
 
     /**
     * the behaviour of the drawing a box with the right-mouse
     *   zoom : zoom on the plot
-    *   box  : (default) send a mouse box event
+    *   box:  (default) send a mouse box event
     */
-    rightclick_rubberbox_mode? : string,
+    rightclick_rubberbox_mode?:  string,
+    rightclick_rubberbox_action?:  string,
 
         /**
     * the type of line to draw for data-points
@@ -172,77 +191,83 @@ export interface IConstructorOptions {
     *   2 : horizontals, horizontal lines are displayed through each point
     *   3 : (default) lines are drawn connecting each point
     */
-    line? : number,
+    line?:  number,
 
     /**
     * autoscaling for the y-axis
-    *    0, "fix"  : axis is fixed to ymin and ymax
-    *    1, "min"  : maximum is fixed, minimum is automatic
-    *    2, "max"  : minimum is fixed, maximum is automatic
+    *    0, "fix":  axis is fixed to ymin and ymax
+    *    1, "min":  maximum is fixed, minimum is automatic
+    *    2, "max":  minimum is fixed, maximum is automatic
     *    3, "auto" : both minimum and maximum are automatic
     */
-    autoy? : string | number,
+    autoy?:  string | number,
 
     /**
     * the units for the y axis (looked up using m.UNITS)
     */
-    ylab? : string | number,
+    ylab?:  string | number,
 
     /**
     * the units for the y axis
     */
-    ylabel? : string | ((...args: any[]) => string),
+    ylabel?:  string | ((...args: any[]) => string),
 
     /**
     * the y-axis minimum value
     */
-    ymin? : number,
+    ymin?:  number,
 
     /**
     * the y-axis maximum value
     */
-    ymax? : number,
+    ymax?:  number,
 
     /**
     * the number of divisions to use on the y-axis
     */
-    ydiv? : number,
+    ydiv?:  number,
 
     /**
     * the z-axis minimum value
     */
 
-    zmin? : number,
+    zmin?:  number,
 
     /**
     * the z-axis maximum value
     */
-    zmax? : number,
+    zmax?:  number,
 
     /**
     * set to true to invert the y-axis
     */
-    yinv? : boolean,
+    yinv?:  boolean,
 
     /**
     * the colors to use for the plot area
     */
-    colors? : IPlotColors,
+    colors?:  IPlotColors,
+
+    /**
+     * Color mapping
+     */
+    cmap?:  number | string | m.IColorMap,
+
 
     /**
     * set to true to use inverted foreground/background colors
     */
-    xi? : boolean,
+    xi?:  boolean,
 
     /**
     * set to true to show all the plot data instead of just the first buffer
     */
-    all? : boolean,
+    all?:  boolean,
 
     /**
     * autoscale the plot based off all the data on not just the first buffer
     */
-    expand? : boolean
+    expand?:  boolean
 
     /**
     * set the origin for the plot
@@ -251,22 +276,30 @@ export interface IConstructorOptions {
          * 3 : x1:xmax, x2:xmin, y1:ymin, y2:ymax (x & y inverted)
          * 4 : x1:xmin, x2:xmax, y1:ymin, y2:ymax (y inverted)
     */
-    origin? : number,
+    origin?:  number,
 
     /**
      * the maximum buffer size to use
      */
-    bufmax? : number,
+    bufmax?:  number,
 
     /**
      * disable handling of keypresses on the plot
      */
-    nokeypress? : boolean,
+    nokeypress?:  boolean,
 
     /**
      * the font family to use for the plot
      */
-    font_family? : string,
+    font_family?:  string,
+
+    /**
+     * Let the wheel control zooming.  Percent controls the speed of the zoom.
+     */
+    wheelZoom?:  boolean;
+    wheelZoomPercent?:  number;
+
+    smoothing?: boolean;
 }
 
 /**
@@ -318,60 +351,62 @@ export interface IGridStyle {
 /**
  * Purpose: Settings Options interface
  * Note:    This settings structure is for updating an already-instantiated
- *          plot.
+ *          plot.  Per M. Ihde, these are a subset of ConstructorOptions
  */
 export interface ISettingsOptions {
-    xyKeys?                      : string;
-    grid?                        : boolean;
-    gridBackground?              : string | Array<string>;  // HTML5 canvas fillStyle (so "red" or an array, etc.)
-    gridStyle?                   : IGridStyle;
-    autol?                       : boolean; // TODO: Define
-    index?                       : boolean;
-    all?                         : boolean;
-    show_x_axis?                 : boolean;
-    show_y_axis?                 : boolean;
-    show_readout?                : boolean;
-    specs?                       : boolean;
-    xcnt?                        : string;  // leftmouse, continuous, disable, enable
-    legend?                      : boolean;
-    pan?                         : boolean;
-    cross?                       : boolean;
-    rubberbox_action?            : string;
-    rubberbox_mode?              : string;
-    rightclick_rubberbox_action? : string;
-    rightclick_rubberbox_mode?   : string;
-    wheelscroll_mode_natural?    : string;
-    wheelZoom?                   : boolean;
-    wheelZoomPercent?            : number;
-    cmode?                       : string;
-    phunits?                     : string;
-    colors?                      : IPlotColors;
-    cmap?                        : number | string | m.IColorMap; // See m.Mc.colormap's array (index, string name, or your own).
-    rasterSmoothing?             : boolean; // null toggle
-    invert?                      : boolean; // null toggle
-    nomenu?                      : boolean; // null toggle
+    xyKeys?:                       string;
+    grid?:                         boolean;
+    gridBackground?:               string | Array<string>;  // HTML5 canvas fillStyle (so "red" or an array, etc.)
+    gridStyle?:                    IGridStyle;
+    fillStyle?:                    string | Array<string>;
+    autol?:                        string | number; // Seems to be like autox, autoy, etc.
+    index?:                        boolean;
+    drawmode?:                     string; // Enumeration rising, falling, scrolling, lefttoright, righttoleft
+    all?:                          boolean;
+    show_x_axis?:                  boolean;
+    show_y_axis?:                  boolean;
+    show_readout?:                 boolean;
+    specs?:                        boolean;
+    xcnt?:                         string;  // leftmouse, continuous, disable (null), enable
+    legend?:                       boolean;
+    pan?:                          boolean;
+    cross?:                        boolean;
+    rubberbox_action?:             string;
+    rubberbox_mode?:               string;
+    rightclick_rubberbox_action?:  string;
+    rightclick_rubberbox_mode?:    string;
+    wheelscroll_mode_natural?:     string;
+    wheelZoom?:                    boolean;
+    wheelZoomPercent?:             number;
+    cmode?:                        string;
+    phunits?:                      string;
+    colors?:                       IPlotColors;
+    cmap?:                         number | string | m.IColorMap; // See m.Mc.colormap's array (index, string name, or your own).
+    rasterSmoothing?:              boolean; // null toggle
+    invert?:                       boolean; // null toggle
+    nomenu?:                       boolean; // null toggle
 
-    note?                        : object; // TODO: Define
-    lg_colorbar?                 : object; // TODO: Define
-    enabled_streaming_pcut?      : boolean;
-    p_cuts?                      : boolean;
+    note?:                         object;  // TODO Define
+    lg_colorbar?:                  boolean;
+    enabled_streaming_pcut?:       boolean;
+    p_cuts?:                       boolean;
 
-    xinv?                        : boolean;
-    xmax?                        : number;
-    xmin?                        : number;
-    autox?                       : number;  // enumeration
-    xcut_now?                    : boolean;
+    xinv?:                         boolean;
+    xmax?:                         number;
+    xmin?:                         number;
+    autox?:                        number | string;  // enumeration
+    xcut_now?:                     boolean;
 
-    yinv?                        : boolean;
-    ymax?                        : number;
-    ymin?                        : number;
-    autoy?                       : number;  // enumeration
-    ycut_now?                    : boolean;
+    yinv?:                         boolean;
+    ymax?:                         number;
+    ymin?:                         number;
+    autoy?:                        number | string;  // enumeration
+    ycut_now?:                     boolean;
 
-    zinv?                        : boolean;
-    zmax?                        : number;
-    zmin?                        : number;
-    autoz?                       : number;  // enumeration
+    zinv?:                         boolean;
+    zmax?:                         number;
+    zmin?:                         number;
+    autoz?:                        number | string;  // enumeration
 }
 
 /**
