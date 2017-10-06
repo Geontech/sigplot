da5aed8aaa6347b288ec569b6f2dcc5166e92419
Thomas Goodwin <btgoodwin@geontech.com>
2017-10-06 14:45:24 -0400

    Added reference to plugins module from sigplot, as now in the js.
    Also added Accordion, Annotation, and Boxes plugins.

---
+++ typings/plugins.d.ts
@@ -0,0 +1,42 @@
+import {
+    AccordionPlugin   as _AccordionPlugin,
+    IAccordionOptions as _IAccordionOptions,
+    IAccordionStyle   as _IAccordionStyle
+} from './sigplot.accordion';
+
+import {
+    AnnotationPlugin   as _AnnotationPlugin,
+    IAnnotationOptions as _IAnnotationOptions,
+    AnnotationValue    as _AnnotationValue,
+    IAnnotation        as _IAnnotation,
+    Annotations        as _Annotations
+} from './sigplot.annotations';
+
+import {
+    BoxesPlugin   as _BoxesPlugin,
+    IBoxesOptions as _IBoxesOptions,
+    IBox          as _IBox
+} from './sigplot.boxes';
+
+// Re-export all plugins and interfaces under the 'plugins' module name.
+export declare module plugins {
+    // Accordion
+    export class     AccordionPlugin   extends _AccordionPlugin {}
+    export interface IAccordionOptions extends _IAccordionOptions {}
+    export interface IAccordionStyle   extends _IAccordionStyle {}
+
+    // Annotation
+    export class     AnnotationPlugin   extends _AnnotationPlugin {}
+    export interface IAnnotationOptions extends _IAnnotationOptions {}
+    export interface IAnnotation        extends _IAnnotation {}
+    export type      Annotations        =       _Annotations;
+
+    // Boxes
+    export class     BoxesPlugin   extends _BoxesPlugin {}
+    export interface IBoxesOptions extends _IBoxesOptions {}
+    export interface IBox          extends _IBox {}
+
+    // Playback (TBD)
+
+    // Slider (TBD)
+}
